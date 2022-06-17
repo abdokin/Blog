a794162440fcc698b5246995dba714c520466373
abdokin <laarichabdo@gmail.com>
2022-06-17 17:54:46 +0100

    add menu

--- web/src/layouts/MainLayout/MainLayout.js
+++ web/src/layouts/MainLayout/MainLayout.js
@@ -4,7 +4,7 @@ const MainLayout = ({ children }) => {
   return (
     <div className="flex min-h-screen w-full flex-col divide-y overflow-hidden bg-black py-6 text-white">
       <Header />
-      <main className="flex-grow justify-items-start gap-10 divide-x overflow-hidden sm:flex">
+      <main className="flex-grow justify-items-start gap-0 divide-x overflow-hidden sm:flex">
         {children}
       </main>
       <Footer />
--- web/src/pages/BlogPage/BlogPage.js
+++ web/src/pages/BlogPage/BlogPage.js
@@ -1,33 +1,71 @@
 import { MetaTags } from '@redwoodjs/web'
 import PostsCell from 'src/components/PostsCell/PostsCell'
-
+import { useState } from 'react'
 const BlogPage = () => {
+  const [open, setopen] = useState(true)
   return (
     <>
       <MetaTags>
         <title>Blog</title>
       </MetaTags>
-      <section className="flex w-full max-w-screen-sm flex-col gap-3 p-4 sm:w-2/3 md:w-1/2">
-        <figure className="bg-slate-100 p-8 dark:bg-black">
-          <img
-            className="mx-auto h-24 w-24 rounded-full"
-            src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
-            alt=""
-          />
-          <div className="space-y-4 pt-6">
-            <blockquote>
-              <p className="text-sm font-normal sm:text-base sm:font-medium md:text-lg lg:text-xl 2xl:text-2xl">
-                “Tailwind CSS is the only framework that I've seen scale on
-                large teams. It’s easy to customize, adapts to any design, and
-                the build size is tiny.”
-              </p>
-            </blockquote>
-            <figcaption className="font-medium">
-              <div className="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
-              <div cla>Staff Engineer, Algolia</div>
-            </figcaption>
-          </div>
-        </figure>
+      <section
+        className={`flex ${
+          open ? 'sm:w-2/3 md:w-1/2 w-full' : 'w-30'
+        }  max-w-screen-sm flex-col gap-3 p-4  relative text-center duration-300`}
+      >
+        {open ? (
+          <>
+            <div className="absolute left-full inset-1 ">
+              <button
+                onClick={() => setopen(!open)}
+                className="bg-white text-black rounded-full text-sm"
+              >
+                click me
+              </button>
+            </div>
+            <figure className="bg-slate-100 p-8 dark:bg-black">
+              <img
+                className="mx-auto h-24 w-24 rounded-full"
+                src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
+                alt=""
+              />
+              <div className="space-y-4 pt-6">
+                <blockquote>
+                  <p className="text-sm font-normal sm:text-base sm:font-medium md:text-lg lg:text-xl 2xl:text-2xl">
+                    “Tailwind CSS is the only framework that I've seen scale on
+                    large teams. It’s easy to customize, adapts to any design,
+                    and the build size is tiny.”
+                  </p>
+                </blockquote>
+                <figcaption className="font-medium">
+                  <div className="text-sky-500 dark:text-sky-400">
+                    Sarah Dayan
+                  </div>
+                  <div cla>Staff Engineer, Algolia</div>
+                </figcaption>
+              </div>
+            </figure>
+          </>
+        ) : (
+          <>
+            <figure className="bg-slate-100  dark:bg-black">
+              <button onClick={() => setopen(!open)}>
+                <img
+                  className="mx-auto h-24 w-24 rounded-full object-cover"
+                  src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
+                  alt=""
+                />
+                <div className=" pt-2">
+                  <figcaption className="font-medium text-sm">
+                    <div className="text-sky-500 dark:text-sky-400">
+                      Sarah Dayan
+                    </div>
+                  </figcaption>
+                </div>
+              </button>
+            </figure>
+          </>
+        )}
       </section>
       <section className="w-full">
         <div className="container my-12 mx-auto px-4 md:px-12 ">
